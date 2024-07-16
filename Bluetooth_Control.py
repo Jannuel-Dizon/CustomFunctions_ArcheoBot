9cd1c1badc0b1603b25e855a6cba1cbb2d329c12
Jannuel Lauro T. Dizon <68643069+Jannuel-Dizon@users.noreply.github.com>
2024-07-16 08:02:31 +0800

    fixed indentation

--- Bluetooth_Control.py
+++ Bluetooth_Control.py
@@ -19,8 +19,8 @@ import mecanum_ArcheoBot as mecanum
 
 
 if sys.version_info.major == 2:
-    print('Please run this program with python3!')
-    sys.exit(0)
+	print('Please run this program with python3!')
+	sys.exit(0)
 """
 camera = PiCamera()
 camera.resolution = (320, 240)
@@ -40,66 +40,64 @@ for joystick in joysticks:
 
 lab_data = None
 def load_config():
-    global lab_data, servo_data
+	global lab_data, servo_data
 
-    lab_data = yaml_handle.get_yaml_data(yaml_handle.lab_file_path)
+	lab_data = yaml_handle.get_yaml_data(yaml_handle.lab_file_path)
 
 def initMove():
-    chassis.set_velocity(50,1)
-    time.sleep(5)
-    chassis.set_velocity(50,-1)
-    time.sleep(5)
-    chassis.set_velocity(100,0)
-    time.sleep(5)
-    chassis.set_velocity(-100,0)
-    time.sleep(5)
+	chassis.set_velocity(0,1)
+	time.sleep(5)
+	chassis.set_velocity(0,-1)
+	time.sleep(5)
+	chassis.set_velocity(100,0)
+	time.sleep(5)
 
 # set buzzer 
 def setBuzzer(timer):
-    Board.setBuzzer(0)
-    Board.setBuzzer(1)
-    time.sleep(timer)
-    Board.setBuzzer(0)
+	Board.setBuzzer(0)
+	Board.setBuzzer(1)
+	time.sleep(timer)
+	Board.setBuzzer(0)
 
 _stop = False
 __isRunning = False
 
 # Reset variable
 def reset(): 
-    global _stop
+	global _stop
 
-    _stop = False
+	_stop = False
 
 # APP initialized calling
 def init():
-    print("Bluetooth Control Init")
-    load_config()
-    initMove()
+	print("Bluetooth Control Init")
+	load_config()
+	initMove()
 
 # APP starts game calling
 def start():
-    global __isRunning
-    reset()
-    __isRunning = True
-    print("Bluetooth Control Start")
+	global __isRunning
+	reset()
+	__isRunning = True
+	print("Bluetooth Control Start")
 
 # APP stops game calling
 def stop():
-    global _stop
-    global __isRunning
-    _stop = True
-    __isRunning = False
-    print("Bluetooth Control Stop")
+	global _stop
+	global __isRunning
+	_stop = True
+	__isRunning = False
+	print("Bluetooth Control Stop")
 
 # APP exits game calling
 def exit():
-    global _stop
-    global __isRunning
-    _stop = True
-    __isRunning = False
-    chassis.set_velocity(0, 0)
-    print("Bluetooth Control Exit")
-    sys.exit(0)
+	global _stop
+	global __isRunning
+	_stop = True
+	__isRunning = False
+	chassis.set_velocity(0, 0)
+	print("Bluetooth Control Exit")
+	sys.exit(0)
 
 
 def move():
@@ -145,45 +143,45 @@ def move():
 # th.setDaemon(True)
 
 def run(img):
-    global __isRunning
-     
-    if not __isRunning:  # Detect whether the game is started, if not, return the original image.
-        return img
-    else:
-        # result = model.predict(img, conf=0.5)
-        # annotated_frame = result[0].plot()
-        return annotated_frame
+	global __isRunning
+	 
+	if not __isRunning:  # Detect whether the game is started, if not, return the original image.
+		return img
+	else:
+		# result = model.predict(img, conf=0.5)
+		# annotated_frame = result[0].plot()
+		return annotated_frame
 
 if __name__ == '__main__':
-    # Initialize the robot
-    init()
-    start()
-    
-    # Set-up Handler to stop running thread
-    signal.signal(signal.SIGINT, exit)
+	# Initialize the robot
+	init()
+	start()
 
-    # Start the thread
-    th = threading.Thread(target=move)
-    th.setDaemon(True)
-    th.start()
+	# Set-up Handler to stop running thread
+	signal.signal(signal.SIGINT, exit)
 
-    # Video capture from usb cam
-    cap = cv2.VideoCapture('http://127.0.0.1:8080?action=stream')
-    while True:
-        ret,img = cap.read()
-        if ret:
-            frame = img.copy()
-            # Frame = run(frame)  
-            # frame_resize = cv2.resize(Frame, (320, 240))
-            # cv2.imshow('frame', frame_resize)
-            key = cv2.waitKey(1)
-        if key == 27:
-            break
-        else:
-            time.sleep(0.01)
-        """
+	# Start the thread
+	th = threading.Thread(target=move)
+	th.setDaemon(True)
+	th.start()
+
+	# Video capture from usb cam
+	cap = cv2.VideoCapture('http://127.0.0.1:8080?action=stream')
+	while True:
+		ret,img = cap.read()
+		if ret:
+			frame = img.copy()
+			# Frame = run(frame)  
+			# frame_resize = cv2.resize(Frame, (320, 240))
+			# cv2.imshow('frame', frame_resize)
+			key = cv2.waitKey(1)
+		if key == 27:
+			break
+		else:
+			time.sleep(0.01)
+		"""
 		For Raspberry camera
-        for img in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+		for img in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
 			# grab the raw NumPy array representing the image, then initialize the timestamp
 			# and occupied/unoccupied text
 			frame = img.array
@@ -194,9 +192,9 @@ if __name__ == '__main__':
 			rawCapture.truncate(0)
 			if key == 27:
 				break
-        """
+		"""
 
-    cap.release()
-    cv2.destroyAllWindows()
-    th.join
+	cap.release()
+	cv2.destroyAllWindows()
+	th.join
 
